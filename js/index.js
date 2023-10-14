530bfcc4e1be090371a2ee76b71bd1c2ebadf80c
José Bauer <jose.h.bauer03@gmail.com>
2023-10-14 16:14:37 -0300

    Adiciona estilos na página 'car'

--- js/index.js
+++ js/index.js
@@ -2,10 +2,10 @@ const navbar = document.getElementById('navbar')
 
 document.addEventListener('scroll', function () {
   if(scrollY > 0) {
-    navbar.classList.add('bg-dark')
+    navbar.classList.add('bg-black')
     navbar.classList.remove('bg-transparent')
 
   } else {
-    navbar.classList.remove('bg-dark')
+    navbar.classList.remove('bg-black')
   }
 })
